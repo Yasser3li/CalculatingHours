3cadef72b9f1702acc760b5612453ab92ea61cbe
YASSER ALI <79377003+Yasser3li@users.noreply.github.com>
2023-08-25 03:21:12 +0300

    Update TimeCalculator.jsx

--- src/components/TimeCalculator.jsx
+++ src/components/TimeCalculator.jsx
@@ -1,18 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./TimeCalculator.css"; // Import the CSS
 
 const TimeCalculator = () => {
   const [arrivalTime, setArrivalTime] = useState("");
-  const [leavingTime, setLeavingTime] = useState(""); // New State for Leaving Time
+  const [leavingTime, setLeavingTime] = useState("");
   const [workedTime, setWorkedTime] = useState("");
   const [darkMode, setDarkMode] = useState(false);
 
-  const calculateWorkedTime = () => {
+  const arrivalTimeRef = useRef(null); // Create a ref for the arrival time input field
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "c" || e.key === "C") {
+        setArrivalTime("");
+        setLeavingTime("");
+        setWorkedTime("");
+        arrivalTimeRef.current.focus(); // Set focus back to the first input field
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  const calculateWorkedTime = (e) => {
+    e.preventDefault();
+
     const enteredArrivalHour = Math.floor(arrivalTime / 100);
     const enteredArrivalMinute = arrivalTime % 100;
-
-    const enteredLeavingHour = Math.floor(leavingTime / 100); // New Code
-    const enteredLeavingMinute = leavingTime % 100; // New Code
+    const enteredLeavingHour = Math.floor(leavingTime / 100);
+    const enteredLeavingMinute = leavingTime % 100;
 
     let workedHours = enteredLeavingHour - enteredArrivalHour;
     let workedMinutes = enteredLeavingMinute - enteredArrivalMinute;
@@ -28,19 +47,22 @@ const TimeCalculator = () => {
   return (
     <div className={darkMode ? "dark-mode" : "light-mode"}>
       <h1>Work Time Calculator</h1>
-      <input
-        type="number"
-        placeholder="Arrival time"
-        value={arrivalTime}
-        onChange={(e) => setArrivalTime(e.target.value)}
-      />
-      <input
-        type="number"
-        placeholder="Leaving time" // New Input Field
-        value={leavingTime}
-        onChange={(e) => setLeavingTime(e.target.value)}
-      />
-      <button onClick={calculateWorkedTime}>Calculate</button>
+      <form onSubmit={calculateWorkedTime}>
+        <input
+          ref={arrivalTimeRef} // Use the ref here
+          type="number"
+          placeholder="Enter your arrival time (e.g., 915)"
+          value={arrivalTime}
+          onChange={(e) => setArrivalTime(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Enter your leaving time (e.g., 1700)"
+          value={leavingTime}
+          onChange={(e) => setLeavingTime(e.target.value)}
+        />
+        <button type="submit">Calculate</button>
+      </form>
       <p>Time worked: {workedTime}</p>
       <button onClick={() => setDarkMode(!darkMode)}>
         Toggle {darkMode ? "Light" : "Dark"} Mode
